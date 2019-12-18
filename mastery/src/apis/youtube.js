0f850ac507fe407436e60e0ee8b02b8b756261e9
Jsparks1989 <jsparks19891@gmail.com>
2019-12-17 22:24:44 -0500

    Set up youtube.js api connection file for the most part.

--- mastery/src/apis/youtube.js
+++ mastery/src/apis/youtube.js
@@ -1,6 +1,18 @@
-
-
-
-
+import axios from 'axios';
 
 const KEY = AIzaSyAA9SfrI-ORNC-hAetkwQ_7eSS3UMrJJFc;
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
+
+
+
+
+
+
